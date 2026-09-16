93d4eb4f4fbd95fc638d67133d57b2a35a1a45ba
Wania8462 <65669738+Wania8462@users.noreply.github.com>
2026-09-16 20:03:27 +0100

    Windows are now dragable

--- Assets/Scripts/Window.cs
+++ Assets/Scripts/Window.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum FieldType
 {
@@ -8,25 +9,75 @@ public enum FieldType
     Dropdown
 }
 
-public struct Field
+public class Field
 {
-    public float name;
-    public FieldType fieldType; 
+    public string name;
+    public FieldType fieldType;
     public Type dataType;
-    public int height;
+
+    public Field(string name, FieldType fieldType, Type dataType)
+    {
+        this.name = name;
+        this.fieldType = fieldType;
+        this.dataType = dataType;
+    }
+
+    public int GetHeight()
+    {
+        return fieldType switch
+        {
+            FieldType.Numeric => 300,
+            FieldType.Dropdown => 200,
+            _ => throw new Exception($"There is no height for {fieldType}"),
+        };
+
+    }
 }
 
 public class Window : MonoBehaviour
 {
-    public List<Field> fields;
+    [SerializeField] private RectTransform background;
 
-    public Window(List<Field> fields)
+    public List<Field> fields = new();
+
+    [SerializeField] private bool topBarHeld;
+    private Vector3 mouseOffset;
+
+    private void Update()
     {
-        this.fields = fields;
+        if (Input.GetMouseButtonUp(0))
+            topBarHeld = false;
+
+        if (topBarHeld)
+            OnTopBarHold();
+    } 
+
+    public void UpdateFields()
+    {
+        int totalHeight = 0;
+
+        foreach (Field field in fields)
+        {
+            totalHeight += field.GetHeight();
+        }
+
+        background.pivot = new Vector2(background.pivot.x, 1f);
+        background.anchorMin = new Vector2(background.anchorMin.x, 1f);
+        background.anchorMax = new Vector2(background.anchorMax.x, 1f);
+        background.anchoredPosition = new Vector2(background.anchoredPosition.x, 0f);
+        background.sizeDelta = new Vector2(background.sizeDelta.x, totalHeight);
     }
 
-    private void OnEnable()
+    public void OnTopBarClick()
     {
-
+        topBarHeld = true;
+        Vector3 mousePos = Input.mousePosition;
+        mouseOffset = transform.position - mousePos;
+        Debug.Log("CLICK!");
+    }
+    
+    private void OnTopBarHold()
+    {
+        transform.position = Input.mousePosition + mouseOffset;
     }
 }
